4275b82f2dea383dcd70405279e01198d90b480e
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-07 19:38:40 -0400

    installed axios, created useEffect hook with axios get request, created new js file then reconsidered if I need it, might delete it later

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,9 +13,20 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.py4e.com/api/people/2/')
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      
     </div>
   );
 }
